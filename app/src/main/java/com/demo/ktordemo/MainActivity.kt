88a91c0ef48dcc95925e8b191bf17dff8b6efee3
Abhishek Srivastava <capgemini.s.srivastava@daimler.com>
2022-10-25 15:16:41 +0530

    Commit fix

--- app/src/main/java/com/demo/ktordemo/MainActivity.kt
+++ app/src/main/java/com/demo/ktordemo/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.demo.ktordemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.demo.ktordemo.ktor_client.api.UserApi
 import com.demo.ktordemo.ktor_client.api.UserRepositoryImpl
 import com.demo.ktordemo.ktor_client.httpClientAndroid
-import io.ktor.client.request.*
+import com.demo.ktordemo.ktor_client.model.UserEntity
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
+
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Serializing objects
+        val data = UserEntity(1,1,"Abhishek")
+        val string = Json.encodeToString(data)
+        println(string)
+        val obj = Json.decodeFromString<UserEntity>(string)
+        println(obj)
+
        GlobalScope.async {
            getUserByName("meelad")
        }
--- app/src/main/java/com/demo/ktordemo/ktor_client/model/UserEntity.kt
+++ app/src/main/java/com/demo/ktordemo/ktor_client/model/UserEntity.kt
@@ -1,9 +1,14 @@
 package com.demo.ktordemo.ktor_client.model
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encoding.*
 
-@Serializable
+@Serializable(with = UserEntitySerializer::class)
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(UserEntity::class)
 data class UserEntity(
                         @SerialName("age")
                         val age: Int,
@@ -11,3 +16,39 @@ data class UserEntity(
                         val count: Int,
                         @SerialName("name")
                         val name: String,)
+
+class UserEntitySerializer: KSerializer<UserEntity>{
+    override fun deserialize(decoder: Decoder): UserEntity = decoder.decodeStructure(descriptor) {
+        var age = -1
+        var count = -1
+        var name = ""
+
+
+        while (true) {
+            when (val index = decodeElementIndex(descriptor)) {
+                0 -> age = decodeIntElement(descriptor, 0)
+                1 -> count = decodeIntElement(descriptor, 1)
+                2 -> name = decodeStringElement(descriptor, 2)
+                CompositeDecoder.DECODE_DONE -> break
+                else -> error("Unexpected index: $index")
+            }
+        }
+        UserEntity(age, count, name)
+    }
+
+    override val descriptor: SerialDescriptor
+        get() =  buildClassSerialDescriptor("UserEntity") {
+            element<Int>("age")
+            element<Int>("count")
+            element<String>("name")
+        }
+
+    override fun serialize(encoder: Encoder, value: UserEntity) {
+        encoder.encodeStructure(descriptor) {
+            encodeIntElement(descriptor, 0, value.age)
+            encodeIntElement(descriptor, 1, value.count)
+            encodeStringElement(descriptor, 2, value.name)
+        }
+    }
+
+}
